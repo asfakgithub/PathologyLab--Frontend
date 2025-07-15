9df72a46683de9bc5c621867daad847cd8b97c82
asfakgithub <asfakahmed075@gmail.com.com>
2025-07-15 18:08:17 +0530

    Fix: removed type: commonjs to support ES modules

--- src/App.js
+++ src/App.js
@@ -5,24 +5,17 @@ import HomeScreen from './Component/Pages/HomeScreen/homeScreen';
 import Status from './Component/Pages/StatusPage/status';
 import Report from './Component/Pages/ReportPage/report';
 import Prescription from './Component/Pages/Prescription/prescription';
-// import axios from "axios"
 
 function App() {
-  // axios.get("http://localhost:8000/test/get").then(resposne => {
-  //   console.log(resposne)
-  // }).catch(err => {
-  //   console.log(err)
-  // })
-
   return (
     <div className="App">
       <Navbar />
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path='/status' element={<Status />} />
-          <Route path='/report/:id' element={<Report />} />
-          <Route path="/prescription/:id" element={<Prescription />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/status" element={<Status />} />
+        <Route path="/report/:id" element={<Report />} />
+        <Route path="/prescription/:id" element={<Prescription />} />
+      </Routes>
     </div>
   );
 }
